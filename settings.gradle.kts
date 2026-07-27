4a33eae907981dd6bac5df196756a1d7966cb560
ttaanaay <296515992+ttaanaay@users.noreply.github.com>
2026-07-27 19:55:45 +0700

    commit

--- settings.gradle.kts
+++
@@ -1,1 +0,0 @@
-rootProject.name = "btween-server"
--- src/main/kotlin/com/btween/server/exception/AppExceptions.kt
+++
@@ -1,6 +0,0 @@
-package com.btween.server.exception
-
-class ValidationException(message: String) : Exception(message)
-class NotFoundException(message: String) : Exception(message)
-class ConflictException(message: String) : Exception(message)
-class UnauthorizedException(message: String) : Exception(message)
